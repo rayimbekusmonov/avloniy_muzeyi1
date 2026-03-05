3bd82956f583212ffebcf2f6baf8365d86f8760f
Rayimbek <rakhimbekmaksudjonoich@gmail.com>
2026-03-05 16:55:45 +0500

    Monolanguage provided

--- frontend/src/i18n/request.ts
+++ frontend/src/i18n/request.ts
@@ -1,7 +1,7 @@
 import {getRequestConfig} from 'next-intl/server';
 
 export default getRequestConfig(async ({requestLocale}) => {
-    const locale = (await requestLocale()) ?? 'uz';
+    const locale = (await requestLocale) ?? 'uz';
     const validLocale = ['uz', 'ru', 'en'].includes(locale) ? locale : 'uz';
 
     return {
